57a554fbd8b1398f05eaf3cf7f1fc7efa7c225b7
Ilya P <ilya63001990@mail.ru>
2026-09-04 00:15:47 +0300

    1

--- src/teorikaModule.js
+++ src/teorikaModule.js
@@ -2,9 +2,9 @@ import { teorikaConfig } from "./service/api/api.config.js";
 
 import { teorikaReg, teorikaAuth, teorikaInit } from "./service/api/api.request.js";
 
-// import { checkScenarios } from "./popup.js";
+import { checkScenarios } from "./popup.js";
 
-// import { startScheme } from "./leadbotTeorika.js";
+import { startScheme } from "./leadbotTeorika.js";
 
 // import { getСookiesID, getDateCookie } from "./utils/cookies.js";
 
